5a4f67787482c76c51736b1b927e45cdf0892773
Carlos Eduardo <ce83413@gmail.com>
2024-10-04 15:14:35 -0300

    trabalhando com arquivos

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		//CRIANDO UM OBJETO DO TIPO FILE
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		
+//		INSTANCIANDO O SCANNER A PARTIR DO ARQUIVO(file)
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+			
+		}
+	}
+
+}
